060cb6e59dc8e6c3a8742627e4a8b4525f704b1c
Gregory Smith <gregorysmith@Gregorys-MacBook-Air.local>
2018-11-26 17:22:23 -0500

    Created Game Logic using JS

---
+++ memory_game/js/main.js
@@ -0,0 +1,12 @@
+console.log("Up and Running!")
+
+var cardOne = "queen";
+
+var cardTwo = "queen";
+
+var cardThree = "king";
+
+var cardFour = "king";
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardThree);
