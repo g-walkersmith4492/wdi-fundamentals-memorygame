f61b9fcad7df9703343771f8fdd3b09dd9f4b721
Gregory Smith <gregorysmith@Gregorys-MacBook-Air.local>
2018-12-03 15:54:44 -0500

    finished creating game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [ 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 
 var cardsInPlay = [];
 
@@ -11,13 +32,28 @@ var checkForMatch = function() {
 }
 }
 
-var flipCard = function(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id')
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
 	checkForMatch()
+
 }
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img')
+		cardElement.setAttribute('src', 'images/back.png')
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard)
+		document.getElementById('game-board').appendChild(cardElement)
+	}
+}
+
+
+createBoard();
