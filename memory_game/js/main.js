4331b69d317d87a1d52cc1e995b3b5cf6da4ab45
Gregory Smith <gregorysmith@Gregorys-MacBook-Air.local>
2018-12-03 16:32:59 -0500

    final syntax changes

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,27 +33,28 @@ var checkForMatch = function() {
 }
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id')
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage)
-	console.log(cards[cardId].suit)
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
 	this.setAttribute('src', cards[cardId].cardImage)
 	if (cardsInPlay.length === 2) {
-	checkForMatch()
+	checkForMatch();
 
 }
 }
 
 var createBoard = function () {
 	for (i = 0; i < cards.length; i++) {
-		var cardElement = document.createElement('img')
-		cardElement.setAttribute('src', 'images/back.png')
-		cardElement.setAttribute('data-id', i)
-		cardElement.addEventListener('click', flipCard)
-		document.getElementById('game-board').appendChild(cardElement)
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
+
 
 
 createBoard();
